40f905544562365948a93f72c78de5614cff9a1c
Volodymyr Volynets <volodymyr.volynets@gmail.com>
2018-03-17 13:54:42 -0400

    owner collection

--- Users/Form/Owner/Collection.php
+++ Users/Form/Owner/Collection.php
@@ -9,45 +9,48 @@ class Collection extends \Object\Form\Wrapper\Collection {
 		$linked_type_code = $options['input']['__linked_type_code'];
 		$linked_module_id = (int) $options['input']['__linked_module_id'];
 		$linked_id = (int) $options['input']['__linked_id'];
-		$this->data = [
-			self::MAIN_SCREEN => [
-				'options' => [
-					'type' => 'forms',
-				],
+		// render form if we have records
+		if (!empty($linked_id)) {
+			$this->data = [
+				self::MAIN_SCREEN => [
+					'options' => [
+						'type' => 'forms',
+					],
+					'order' => 1000,
+					self::ROWS => []
+				]
+			];
+			$this->data[self::MAIN_SCREEN][self::ROWS]['row1'] = [
 				'order' => 1000,
-				self::ROWS => []
-			]
-		];
-		$this->data[self::MAIN_SCREEN][self::ROWS]['row1'] = [
-			'order' => 1000,
-			'options' => [
-				'type' => 'tabs',
-				'segment' => [
-					'type' => 'info',
-					'header' => [
-						'icon' => ['type' => 'far fa-user'],
-						'title' => 'Owners'
-					]
+				'options' => [
+					'type' => 'tabs',
+					'segment' => [
+						'type' => 'info',
+						'header' => [
+							'icon' => ['type' => 'far fa-user'],
+							'title' => 'Owners'
+						]
+					],
+					'its_own_segment' => true
+				]
+			];
+			$input = $options['input'];
+			$input['um_owneruser_linked_type_code'] = $linked_type_code;
+			$input['um_owneruser_linked_module_id'] = $linked_module_id;
+			$input['um_owneruser_linked_id'] = $linked_id;
+			$input['__anchor'] = 'form_um_owners_form_id_form_anchor';
+			$this->data[self::MAIN_SCREEN][self::ROWS]['row1'][self::FORMS]['um_owners_form_id'] = [
+				'model' => '\Numbers\Users\Users\Form\Owner\ExistingOwners',
+				'options' => [
+					'label_name' => 'Existing Owners',
+					'form_link' => 'um_owners_form_id',
+					'percent' => 100,
+					'input' => $input,
+					'bypass_hidden_from_input' => ($options['__parent_options']['bypass_input'] ?? []),
 				],
-				'its_own_segment' => true
-			]
-		];
-		$input = $options['input'];
-		$input['um_owneruser_linked_type_code'] = $linked_type_code;
-		$input['um_owneruser_linked_module_id'] = $linked_module_id;
-		$input['um_owneruser_linked_id'] = $linked_id;
-		$input['__anchor'] = 'form_um_owners_form_id_form_anchor';
-		$this->data[self::MAIN_SCREEN][self::ROWS]['row1'][self::FORMS]['um_owners_form_id'] = [
-			'model' => '\Numbers\Users\Users\Form\Owner\ExistingOwners',
-			'options' => [
-				'label_name' => 'Existing Owners',
-				'form_link' => 'um_owners_form_id',
-				'percent' => 100,
-				'input' => $input,
-				'bypass_hidden_from_input' => ($options['__parent_options']['bypass_input'] ?? []),
-			],
-			'order' => 1
-		];
+				'order' => 1
+			];
+		}
 		// change owners only if you permitted
 		/*
 		if (\Application::$controller->can('Record_Execute_Owners', 'Edit')) {
