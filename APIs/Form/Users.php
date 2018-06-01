b27f4987556e41464fddf88d9f948c55d730d3b4
Volodymyr Volynets <volodymyr.volynets@gmail.com>
2018-06-01 12:48:26 -0400

    API module, API users

---
+++ APIs/Form/Users.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Numbers\Users\APIs\Form;
+class Users extends \Object\Form\Wrapper\Base {
+	public $form_link = 'ua_users';
+	public $module_code = 'UA';
+	public $title = 'U/A Users Form';
+	public $options = [
+		'segment' => self::SEGMENT_FORM,
+		'actions' => [
+			'refresh' => true,
+			'new' => true,
+			'back' => true,
+			'import' => true
+		]
+	];
+	public $containers = [
+		'top' => ['default_row_type' => 'grid', 'order' => 100],
+		'buttons' => ['default_row_type' => 'grid', 'order' => 900],
+	];
+	public $rows = [];
+	public $elements = [
+		'top' => [
+			'ua_apiusr_id' => [
+				'ua_apiusr_id' => ['order' => 1, 'row_order' => 100, 'label_name' => 'User #', 'domain' => 'user_id_sequence', 'percent' => 50, 'navigation' => true],
+				'ua_apiusr_code' => ['order' => 2, 'label_name' => 'Code', 'domain' => 'group_code', 'null' => true, 'percent' => 50, 'required' => 'c', 'navigation' => true]
+			],
+			'ua_apiusr_name' => [
+				'ua_apiusr_name' => ['order' => 1, 'row_order' => 200, 'label_name' => 'Name', 'domain' => 'name', 'percent' => 70, 'required' => true],
+				'ua_apiusr_hold' => ['order' => 2, 'label_name' => 'Hold', 'type' => 'boolean', 'percent' => 15],
+				'ua_apiusr_inactive' => ['order' => 3, 'label_name' => 'Inactive', 'type' => 'boolean', 'percent' => 15]
+			],
+			'ua_apiusr_login_username' => [
+				'ua_apiusr_login_username' => ['order' => 1, 'row_order' => 300, 'label_name' => 'Username', 'domain' => 'login', 'null' => true, 'required' => true, 'percent' => 50],
+				'ua_apiusr_login_password_new' => ['order' => 2, 'label_name' => 'Password', 'domain' => 'password', 'null' => true, 'required' => 'c', 'percent' => 50, 'empty_value' => true],
+			]
+		],
+		'buttons' => [
+			self::BUTTONS => self::BUTTONS_DATA_GROUP
+		]
+	];
+
+	public $collection = [
+		'name' => 'Users',
+		'model' => '\Numbers\Users\APIs\Model\Users',
+	];
+
+	public function validate(& $form) {
+		if (!$form->values_loaded && empty($form->values['ua_apiusr_login_password_new'])) {
+			$form->error(DANGER, \Object\Content\Messages::REQUIRED_FIELD, 'ua_apiusr_login_password_new');
+		}
+		if (!$form->hasErrors()) {
+			if (!empty($form->values['ua_apiusr_login_password_new'])) {
+				$crypt = new \Crypt();
+				$form->values['ua_apiusr_login_password'] = $crypt->passwordHash($form->values['ua_apiusr_login_password_new']);
+			}
+		}
+	}
+}
---
+++ APIs/Model/Users.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Numbers\Users\APIs\Model;
+class Users extends \Object\Table {
+	public $db_link;
+	public $db_link_flag;
+	public $module_code = 'UA';
+	public $title = 'U/A API Users';
+	public $schema;
+	public $name = 'ua_api_users';
+	public $pk = ['ua_apiusr_tenant_id', 'ua_apiusr_id'];
+	public $tenant = true;
+	public $orderby;
+	public $limit;
+	public $column_prefix = 'ua_apiusr_';
+	public $columns = [
+		'ua_apiusr_tenant_id' => ['name' => 'Tenant #', 'domain' => 'tenant_id'],
+		'ua_apiusr_id' => ['name' => 'User #', 'domain' => 'user_id_sequence'],
+		'ua_apiusr_code' => ['name' => 'User Number', 'domain' => 'group_code', 'null' => true],
+		'ua_apiusr_name' => ['name' => 'Name', 'domain' => 'name'],
+		// login
+		'ua_apiusr_login_username' => ['name' => 'Username', 'domain' => 'login', 'null' => true],
+		'ua_apiusr_login_password' => ['name' => 'Password', 'domain' => 'password', 'null' => true],
+		// inactive & hold
+		'ua_apiusr_hold' => ['name' => 'Hold', 'type' => 'boolean'],
+		'ua_apiusr_inactive' => ['name' => 'Inactive', 'type' => 'boolean']
+	];
+	public $constraints = [
+		'ua_api_users_pk' => ['type' => 'pk', 'columns' => ['ua_apiusr_tenant_id', 'ua_apiusr_id']],
+		'ua_apiusr_code_un' => ['type' => 'unique', 'columns' => ['ua_apiusr_tenant_id', 'ua_apiusr_code']],
+		'ua_apiusr_login_username_un' => ['type' => 'unique', 'columns' => ['ua_apiusr_tenant_id', 'ua_apiusr_login_username']],
+	];
+	public $indexes = [];
+	public $history = false;
+	public $audit = [
+		'map' => [
+			'ua_apiusr_tenant_id' => 'wg_audit_tenant_id',
+			'ua_apiusr_id' => 'wg_audit_api_id'
+		]
+	];
+	public $optimistic_lock = true;
+	public $options_map = [
+		'ua_apiusr_name' => 'name',
+		'ua_apiusr_inactive' => 'inactive'
+	];
+	public $options_active = [
+		'ua_apiusr_inactive' => 0
+	];
+	public $options_skip_i18n = true;
+	public $engine = [
+		'MySQLi' => 'InnoDB'
+	];
+
+	public $cache = false;
+	public $cache_tags = [];
+	public $cache_memory = false;
+
+	public $who = [
+		'inserted' => true,
+		'updated' => true
+	];
+
+	public $data_asset = [
+		'classification' => 'client_confidential',
+		'protection' => 2,
+		'scope' => 'enterprise'
+	];
+}
