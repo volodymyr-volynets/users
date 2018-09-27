1db416620045db9d4d02d254d88cd51a55644e3a
Volodymyr Volynets <volodymyr.volynets@gmail.com>
2018-09-27 09:56:36 -0400

    ABAC --- ABAC:	Enhancement \#1041 \(Fixed ABAC presets.\)

--- Organizations/Data/ABAC.php
+++ Organizations/Data/ABAC.php
@@ -16,13 +16,15 @@ class ABAC extends \Object\Import {
 					'sm_abacattr_name' => 'Organization #',
 					'sm_abacattr_module_code' => 'ON',
 					'sm_abacattr_parent_abacattr_id' => null,
+					'sm_abacattr_tenant' => 1,
+					'sm_abacattr_module' => 0,
 					'sm_abacattr_flag_abac' => 1,
 					'sm_abacattr_flag_assingment' => 1,
 					'sm_abacattr_flag_attribute' => 1,
 					'sm_abacattr_model_id' => '::primary_model::\Numbers\Users\Organizations\Model\Organizations',
 					'sm_abacattr_domain' => '::from::columns::on_organization_id::domain',
 					'sm_abacattr_type' => '::from::columns::on_organization_id::type',
-					'sm_abacattr_is_numeric_key' => 'from::columns::on_organization_id::is_numeric_key',
+					'sm_abacattr_is_numeric_key' => '::from::columns::on_organization_id::is_numeric_key',
 					'sm_abacattr_inactive' => 0
 				]
 			]
--- Users/Data/ABAC.php
+++ Users/Data/ABAC.php
@@ -16,13 +16,15 @@ class ABAC extends \Object\Import {
 					'sm_abacattr_name' => 'User #',
 					'sm_abacattr_module_code' => 'UM',
 					'sm_abacattr_parent_abacattr_id' => null,
+					'sm_abacattr_tenant' => 1,
+					'sm_abacattr_module' => 0,
 					'sm_abacattr_flag_abac' => 1,
 					'sm_abacattr_flag_assingment' => 1,
 					'sm_abacattr_flag_attribute' => 1,
 					'sm_abacattr_model_id' => '::primary_model::\Numbers\Users\Users\Model\Users',
 					'sm_abacattr_domain' => '::from::columns::um_user_id::domain',
 					'sm_abacattr_type' => '::from::columns::um_user_id::type',
-					'sm_abacattr_is_numeric_key' => 'from::columns::um_user_id::is_numeric_key',
+					'sm_abacattr_is_numeric_key' => '::from::columns::um_user_id::is_numeric_key',
 					'sm_abacattr_inactive' => 0
 				],
 				[
@@ -31,13 +33,15 @@ class ABAC extends \Object\Import {
 					'sm_abacattr_name' => 'Role #',
 					'sm_abacattr_module_code' => 'UM',
 					'sm_abacattr_parent_abacattr_id' => null,
+					'sm_abacattr_tenant' => 1,
+					'sm_abacattr_module' => 0,
 					'sm_abacattr_flag_abac' => 1,
 					'sm_abacattr_flag_assingment' => 1,
 					'sm_abacattr_flag_attribute' => 1,
 					'sm_abacattr_model_id' => '::primary_model::\Numbers\Users\Users\Model\Roles',
 					'sm_abacattr_domain' => '::from::columns::um_role_id::domain',
 					'sm_abacattr_type' => '::from::columns::um_role_id::type',
-					'sm_abacattr_is_numeric_key' => 'from::columns::um_role_id::is_numeric_key',
+					'sm_abacattr_is_numeric_key' => '::from::columns::um_role_id::is_numeric_key',
 					'sm_abacattr_inactive' => 0
 				]
 			]
